4fa9534f3582833e2165d50988f8186e813208b0
mr-yanghm <code004@gmail.com>
2020-12-30 20:06:13 +0900

    css -> postCSS Module 적용

--- src/components/button1.jsx
+++ src/components/button1.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './button1.css';
+import React, { Component } from "react";
+import styles from "./button1.module.css";
 
 class Button1 extends Component {
-    render() {
-        return (
-            <div className="button">
-                <span className="text">Button1</span>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={styles.button}>
+        <span className={styles.text}>Button1</span>
+      </div>
+    );
+  }
 }
 
 export default Button1;
--- src/components/button2.jsx
+++ src/components/button2.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './button2.css';
+import React, { Component } from "react";
+import styles from "./button2.module.css";
 
 class Button2 extends Component {
-    render() {
-        return (
-            <div className="button">
-                <span className="text">Button2</span>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={styles.button}>
+        <span className={styles.text}>Button2</span>
+      </div>
+    );
+  }
 }
 
 export default Button2;
